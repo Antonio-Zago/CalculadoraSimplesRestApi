2aa9ccd0cecd8da3585a27b99635f953f95d440d
Antonio-Zago <antoniozagodev@gmail.com>
2021-09-29 11:00:22 -0300

    Implementação dos métodos das demais operações matematicas

--- RestWithAspNet/RestWithAspNet/Controllers/CalculatorController.cs
+++ RestWithAspNet/RestWithAspNet/Controllers/CalculatorController.cs
@@ -20,12 +20,85 @@ namespace RestWithAspNet.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult sum(string firstNumber, string secondNumber)
         {
-            if (IsNumeric(firstNumber) && IsNumeric(firstNumber))
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                 return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("mul/{firstNumber}/{secondNumber}")]
+        public IActionResult mul(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mul = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(mul.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                if(ConvertToDecimal(secondNumber) != 0)
+                {
+                    var div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                    return Ok(div.ToString());
+                }
+                else
+                {
+                    return BadRequest("Impossivel divisão por zero");
+                }
+                
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("med/{firstNumber}/{secondNumber}")]
+        public IActionResult med(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+
+                var med = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(med.ToString());
+
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("raiz/{firstNumber}")]
+        public IActionResult raiz(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                if(ConvertToDecimal(firstNumber) < 0)
+                {
+                    return BadRequest("Impossivel raiz de número negativo");
+                }
+
+                var raiz = Math.Sqrt(decimal.ToDouble(ConvertToDecimal(firstNumber)));
+                return Ok(raiz.ToString());
+
+
             }
             return BadRequest("Invalid Input");
         }
